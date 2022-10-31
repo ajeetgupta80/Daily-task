d3a4cb5dfa412cc9aee79ff17579021bfc5f955c
fucking ajeet <ajeet808080@gmail.com>
2022-11-01 02:36:23 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+class App extends Component{
+  render()
+  {
+    return(
+      <div className="container-fluid my-5">
+        <div className="row">
+          <div className="col-sm-6 mx-auto text-black shadow-lg p-2">
+            <h1 className="text-center "> DAILY TASK </h1>
+            <div className="row">
+              <div className="col-9">
+                <input type="text" className='form-control' placeholder='write your task ' />
+              </div>
+              <div className="col-2">
+                <button className='btn btn-warning px-5 font-weight-bold '> Add </button>
+
+              </div>
+            </div>
+
+          </div>
+        </div>   
+      </div>
+    )
+  }
 }
 
 export default App;
